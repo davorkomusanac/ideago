dc3b9bc01b683e562c7e0674f0334cbb4168f45f
Davor Komusanac <davor.komusanac@gmail.com>
2022-06-25 01:38:05 +0200

    moved ideaTextField Label to separate widget and above Textfield in UI

---
+++ lib/presentation/widgets/idea_textfield_label.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class IdeaTextFieldLabel extends StatelessWidget {
+  final String label;
+  final double leftPadding;
+  final double bottomPadding;
+
+  const IdeaTextFieldLabel({
+    required this.label,
+    this.leftPadding = 28.0,
+    this.bottomPadding = 2.0,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.only(
+          left: leftPadding,
+          bottom: bottomPadding,
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+}
