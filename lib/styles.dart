d5035431a4e4b5151416558eee23f4e2756be118
Davor Komusanac <davor.komusanac@gmail.com>
2022-06-25 01:37:02 +0200

    create styles file

---
+++ lib/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final ButtonStyle ideaStatusBottomSheetStyle = TextButton.styleFrom(
+  primary: Colors.black,
+  textStyle: const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  ),
+);
