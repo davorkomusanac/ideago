c52d58d9e9ea2b75abf68e596e7e5eb514841b64
Davor Komusanac <davor.komusanac@gmail.com>
2022-07-04 23:49:27 +0200

    add IsarIdeaRatingQuestionConverter and some refactoring

--- lib/presentation/pages/rate_idea/rate_idea_page.dart
+++ lib/presentation/pages/rate_idea/rate_idea_page.dart
@@ -8,77 +8,69 @@ import 'widgets/rate_idea_outro.dart';
 import 'widgets/rate_idea_slider_header.dart';
 
 class RateIdeaPage extends StatelessWidget {
-  const RateIdeaPage({
-    required this.cubit,
-    Key? key,
-  }) : super(key: key);
-
-  final RateIdeaCubit cubit;
+  const RateIdeaPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => BlocProvider<RateIdeaCubit>.value(
-        value: cubit,
-        child: Scaffold(
-          //TODO Change design and UI of page
-          backgroundColor: Colors.blueGrey[800],
-          appBar: AppBar(
-            title: const Text('Rate idea'),
-            backgroundColor: Colors.teal,
-          ),
-          body: BlocBuilder<RateIdeaCubit, RateIdeaState>(
-            builder: (context, state) => ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: state.questionRatings.length + 3,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return const RateIdeaIntro();
-                } else if (index == state.questionRatings.length + 1) {
-                  return const RateIdeaOutro();
-                } else if (index == state.questionRatings.length + 2) {
-                  return ConfirmRatingButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                  );
-                } else {
-                  var questionTitle = state.questionRatings[index - 1].title;
-                  var questionDescription = state.questionRatings[index - 1].description;
-                  var questionRating = state.questionRatings[index - 1].rating;
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: RichText(
-                          text: TextSpan(
-                            text: '$index. $questionTitle',
-                            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
-                            children: [
-                              TextSpan(
-                                text: ' - $questionDescription',
-                                style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
-                              )
-                            ],
-                          ),
+  Widget build(BuildContext context) => Scaffold(
+        //TODO Change design and UI of page
+        backgroundColor: Colors.blueGrey[800],
+        appBar: AppBar(
+          title: const Text('Rate idea'),
+          backgroundColor: Colors.teal,
+        ),
+        body: BlocBuilder<RateIdeaCubit, RateIdeaState>(
+          builder: (context, state) => ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: state.questionRatings.length + 3,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return const RateIdeaIntro();
+              } else if (index == state.questionRatings.length + 1) {
+                return const RateIdeaOutro();
+              } else if (index == state.questionRatings.length + 2) {
+                return ConfirmRatingButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                );
+              } else {
+                var questionTitle = state.questionRatings[index - 1].title;
+                var questionDescription = state.questionRatings[index - 1].description;
+                var questionRating = state.questionRatings[index - 1].rating;
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: '$index. $questionTitle',
+                          style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                          children: [
+                            TextSpan(
+                              text: ' - $questionDescription',
+                              style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
+                            )
+                          ],
                         ),
                       ),
-                      RateIdeaSliderHeader(currentSliderValue: questionRating),
-                      Slider.adaptive(
-                        value: questionRating.toDouble(),
-                        min: 1,
-                        max: 10,
-                        divisions: 9,
-                        activeColor: Colors.teal,
-                        onChanged: (newRating) {
-                          cubit.rateQuestion(
-                            questionIndex: index - 1,
-                            questionRating: newRating.round(),
-                          );
-                        },
-                      ),
-                    ],
-                  );
-                }
-              },
-            ),
+                    ),
+                    RateIdeaSliderHeader(currentSliderValue: questionRating),
+                    Slider.adaptive(
+                      value: questionRating.toDouble(),
+                      min: 1,
+                      max: 10,
+                      divisions: 9,
+                      activeColor: Colors.teal,
+                      onChanged: (newRating) {
+                        context.read<RateIdeaCubit>().rateQuestion(
+                              questionIndex: index - 1,
+                              questionRating: newRating.round(),
+                            );
+                      },
+                    ),
+                  ],
+                );
+              }
+            },
           ),
         ),
       );
--- lib/presentation/widgets/idea_status_bottom_sheet.dart
+++ lib/presentation/widgets/idea_status_bottom_sheet.dart
@@ -6,81 +6,73 @@ import '../../constants.dart';
 import '../../styles.dart';
 
 class IdeaStatusBottomSheet extends StatelessWidget {
-  const IdeaStatusBottomSheet({
-    required this.cubit,
-    Key? key,
-  }) : super(key: key);
-
-  final AddOrUpdateIdeaCubit cubit;
+  const IdeaStatusBottomSheet({Key? key}) : super(key: key);
 
   void statusTapped(String ideaStatus, BuildContext context) {
-    cubit.ideaStatusChanged(ideaStatus);
+    context.read<AddOrUpdateIdeaCubit>().ideaStatusChanged(ideaStatus);
     Navigator.of(context).pop();
     FocusManager.instance.primaryFocus?.unfocus();
   }
 
   @override
-  Widget build(BuildContext context) => BlocProvider<AddOrUpdateIdeaCubit>.value(
-        value: cubit,
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.35,
-          decoration: const BoxDecoration(
-            //TODO Apply colors
-            color: Colors.green,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30.0),
-              topRight: Radius.circular(30.0),
+  Widget build(BuildContext context) => Container(
+        height: MediaQuery.of(context).size.height * 0.35,
+        decoration: const BoxDecoration(
+          //TODO Apply colors
+          color: Colors.green,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            //TODO Change Design
+            Expanded(
+              child: TextButton(
+                style: ideaStatusBottomSheetStyle,
+                onPressed: () => statusTapped(ideaStatusToDo, context),
+                child: const Text(ideaStatusToDo),
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              //TODO Change Design
-              Expanded(
-                child: TextButton(
-                  style: ideaStatusBottomSheetStyle,
-                  onPressed: () => statusTapped(ideaStatusToDo, context),
-                  child: const Text(ideaStatusToDo),
-                ),
+            const Divider(
+              height: 0,
+              thickness: 1,
+              color: Colors.black,
+            ),
+            Expanded(
+              child: TextButton(
+                style: ideaStatusBottomSheetStyle,
+                onPressed: () => statusTapped(ideaStatusInProgress, context),
+                child: const Text(ideaStatusInProgress),
               ),
-              const Divider(
-                height: 0,
-                thickness: 1,
-                color: Colors.black,
+            ),
+            const Divider(
+              height: 0,
+              thickness: 1,
+              color: Colors.black,
+            ),
+            Expanded(
+              child: TextButton(
+                style: ideaStatusBottomSheetStyle,
+                onPressed: () => statusTapped(ideaStatusDone, context),
+                child: const Text(ideaStatusDone),
               ),
-              Expanded(
-                child: TextButton(
-                  style: ideaStatusBottomSheetStyle,
-                  onPressed: () => statusTapped(ideaStatusInProgress, context),
-                  child: const Text(ideaStatusInProgress),
-                ),
+            ),
+            const Divider(
+              height: 0,
+              thickness: 1,
+              color: Colors.black,
+            ),
+            Expanded(
+              child: TextButton(
+                style: ideaStatusBottomSheetStyle,
+                onPressed: () => statusTapped(ideaStatusDiscarded, context),
+                child: const Text(ideaStatusDiscarded),
               ),
-              const Divider(
-                height: 0,
-                thickness: 1,
-                color: Colors.black,
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ideaStatusBottomSheetStyle,
-                  onPressed: () => statusTapped(ideaStatusDone, context),
-                  child: const Text(ideaStatusDone),
-                ),
-              ),
-              const Divider(
-                height: 0,
-                thickness: 1,
-                color: Colors.black,
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ideaStatusBottomSheetStyle,
-                  onPressed: () => statusTapped(ideaStatusDiscarded, context),
-                  child: const Text(ideaStatusDiscarded),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
 }
