e34150c947ef4b2bdec83c9984d92916f47b7dbe
Davor Komusanac <davor.komusanac@gmail.com>
2022-07-14 03:26:54 +0200

    implement Idea Categories functionality (add, delete, check, search)

--- lib/data/models/idea_category/idea_category.dart
+++ lib/data/models/idea_category/idea_category.dart
@@ -3,22 +3,26 @@ import 'package:equatable/equatable.dart';
 class IdeaCategory extends Equatable {
   final String uid;
   final String title;
+  final bool isChecked;
 
   const IdeaCategory({
     required this.uid,
     required this.title,
+    this.isChecked = false,
   });
 
   @override
-  String toString() => 'IdeaCategory{ uid: $uid, title: $title,}';
+  String toString() => 'IdeaCategory{ uid: $uid, title: $title, isChecked: $isChecked,}';
 
   IdeaCategory copyWith({
     String? uid,
     String? title,
+    bool? isChecked,
   }) =>
       IdeaCategory(
         uid: uid ?? this.uid,
         title: title ?? this.title,
+        isChecked: isChecked ?? this.isChecked,
       );
 
   Map<String, dynamic> toJson() => {
