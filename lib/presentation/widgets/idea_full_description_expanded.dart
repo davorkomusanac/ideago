0bd3266bf304e15b6477ab2d7083b6f10a16daf2
Davor Komusanac <davor.komusanac@gmail.com>
2022-07-27 09:31:39 +0200

    refactor expanded full description into separate file

---
+++ lib/presentation/widgets/idea_full_description_expanded.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../application/add_or_update_idea/add_or_update_idea_cubit.dart';
+import '../../constants.dart';
+import 'idea_textfield.dart';
+import 'idea_textfield_label.dart';
+
+class IdeaFullDescriptionExpanded extends StatelessWidget {
+  const IdeaFullDescriptionExpanded({
+    Key? key,
+    required this.fullDescriptionController,
+    required this.descriptionFullScreenFocusNode,
+  }) : super(key: key);
+
+  final TextEditingController fullDescriptionController;
+  final FocusNode descriptionFullScreenFocusNode;
+
+  @override
+  Widget build(BuildContext context) => CustomScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        slivers: [
+          //A scrollview is needed to be able to dismiss keyboard on drag, CustomScrollView works with Expanded
+          SliverFillRemaining(
+            child: Column(
+              //A key is needed here so that AnimatedSwitcher can know the difference between children and animate them
+              key: const ValueKey<int>(0),
+              children: [
+                const IdeaTextFieldLabel(
+                  label: kIdeaTextFieldFullDescriptionLabel,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16.0,
+                      right: 16.0,
+                      bottom: 16.0,
+                    ),
+                    child: Stack(
+                      children: [
+                        IdeaTextField(
+                          focusNode: descriptionFullScreenFocusNode,
+                          controller: fullDescriptionController,
+                          hintText: kIdeaTextFieldFullDescriptionHint,
+                          minLines: null,
+                          maxLines: null,
+                          expands: true,
+                          contentPadding: const EdgeInsets.fromLTRB(12, 12, 12, 36),
+                        ),
+                        Positioned(
+                          right: 0,
+                          bottom: 0,
+                          child: InkWell(
+                            onTap: () => context.read<AddOrUpdateIdeaCubit>().descriptionButtonPressed(),
+                            child: const Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: Icon(Icons.fullscreen_exit),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+}
