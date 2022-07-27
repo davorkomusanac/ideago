60adfa2e0e061e6af11433b801b9964a586af8b2
Davor Komusanac <davor.komusanac@gmail.com>
2022-07-27 23:46:29 +0200

    use Stream to read ideas from offline storage

--- lib/repository/idea/idea_interface.dart
+++ lib/repository/idea/idea_interface.dart
@@ -3,6 +3,8 @@ import '../../data/models/idea/idea.dart';
 abstract class IdeaInterface {
   Future<List<Idea>> getAllIdeas();
 
+  Stream<List<Idea>> getIdeas();
+
   Future<void> addIdea(Idea idea);
 
   Future<void> deleteIdea(Idea idea);
