08237a4f8e5f151645f7d7844e882da20a06c067
Davor Komusanac <davor.komusanac@gmail.com>
2022-08-03 12:29:03 +0200

    implement search functionality

--- lib/application/ideas/ideas_state.dart
+++ lib/application/ideas/ideas_state.dart
@@ -10,39 +10,51 @@ class IdeasState extends Equatable {
   const IdeasState({
     this.status = IdeasStatus.loading,
     this.ideas = const <Idea>[],
+    this.initialLoadedIdeas = const <Idea>[],
     this.errorMessage = '',
     this.errorMessageLoadingIdeas = '',
     this.isThereMoreIdeasToLoad = true,
+    this.searchTerm = '',
   });
 
   final IdeasStatus status;
   final List<Idea> ideas;
+  //An additional List<Idea> needed for real time search, so when search is done, we show back the original ideas
+  final List<Idea> initialLoadedIdeas;
   final String errorMessage;
   //An additional String errorMessage to differentiate when showing a SnackBar error for Idea operations and static view for loading ideas
   final String errorMessageLoadingIdeas;
   final bool isThereMoreIdeasToLoad;
+  //Search term needed as a property to be able to update real time searched ideas
+  final String searchTerm;
 
   IdeasState copyWith({
     IdeasStatus? status,
     List<Idea>? ideas,
+    List<Idea>? initialLoadedIdeas,
     String? errorMessage,
     String? errorMessageLoadingIdeas,
     bool? isThereMoreIdeasToLoad,
+    String? searchTerm,
   }) =>
       IdeasState(
         status: status ?? this.status,
         ideas: ideas ?? this.ideas,
+        initialLoadedIdeas: initialLoadedIdeas ?? this.initialLoadedIdeas,
         errorMessage: errorMessage ?? this.errorMessage,
         errorMessageLoadingIdeas: errorMessageLoadingIdeas ?? this.errorMessageLoadingIdeas,
         isThereMoreIdeasToLoad: isThereMoreIdeasToLoad ?? this.isThereMoreIdeasToLoad,
+        searchTerm: searchTerm ?? this.searchTerm,
       );
 
   @override
   List<Object> get props => [
         status,
         ideas,
+        initialLoadedIdeas,
         errorMessage,
         errorMessageLoadingIdeas,
         isThereMoreIdeasToLoad,
+        searchTerm,
       ];
 }
