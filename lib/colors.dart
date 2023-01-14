1ec1bae8097b39045a5fb667c39f133adb97651f
Davor Komusanac <53110486+davorkomusanac@users.noreply.github.com>
2023-01-14 01:24:25 +0100

    change text selection colors to primaryForegroundColor, change primay color scheme (to influence dialogs) (#3)

--- lib/colors.dart
+++ lib/colors.dart
@@ -11,7 +11,7 @@ class AppColors {
   static const darkRed = Color(0xFFB71C1C);
   static const orange = Color(0xFFFF9800);
   static const green = Color(0xFF4CAF50);
-  static const darkGreen = Color(0xFF00C853);
+  static const lightGreen = Color(0xFF00C853);
   static const grey = Color(0xFF9E9E9E);
   static const yellow = Color(0xFFFFEB3B);
   static const darkYellow = Color(0xFFFBC02D);
