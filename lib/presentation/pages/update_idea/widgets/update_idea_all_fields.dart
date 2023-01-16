0469b91ac66c5f6169c92d6fbd0760988cea6121
Davor Komusanac <davor.komusanac@gmail.com>
2023-01-16 01:54:46 +0100

    always show update idea button inside update idea page

--- lib/presentation/pages/update_idea/widgets/update_idea_all_fields.dart
+++ lib/presentation/pages/update_idea/widgets/update_idea_all_fields.dart
@@ -45,223 +45,229 @@ class UpdateIdeaAllFields extends StatelessWidget {
   final FocusNode descriptionFullScreenFocusNode;
 
   @override
-  Widget build(BuildContext context) => SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        child: Column(
-          children: [
-            ///
-            ///Title
-            ///
-            const SizedBox(height: 10),
-            const IdeaTextFieldLabel(
-              leftPadding: 68,
-              label: kIdeaTextFieldTitleLabel,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(width: 8),
-                const DiscardIdeaButton(
-                  content: kDiscardUpdateIdeaDialogContent,
-                  icon: Icons.arrow_back,
-                ),
-                Expanded(
-                  child: IdeaTextField(
-                    controller: titleController,
-                    hintText: kIdeaTextFieldTitleHint,
-                    margin: const EdgeInsets.only(right: 8.0),
-                  ),
-                ),
-                DeleteIdeaButton(idea: idea),
-                const SizedBox(width: 8),
-              ],
-            ),
-
-            ///
-            ///Summary
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldSummaryLabel,
-            ),
-            IdeaTextField(
-              controller: summaryController,
-              hintText: kIdeaTextFieldSummaryHint,
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-
-            ///
-            ///Description
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldFullDescriptionLabel,
-            ),
-            IdeaFullDescriptionMinimized(
-              fullDescriptionController: fullDescriptionController,
-              descriptionFullScreenFocusNode: descriptionFullScreenFocusNode,
-            ),
-
-            ///
-            ///Status and Rating
-            ///
-            const SizedBox(height: 24),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
+  Widget build(BuildContext context) => Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              child: Column(
                 children: [
-                  IdeaStatusField(statusController: statusController),
-                  const SizedBox(width: 26),
-                  IdeaRatingField(ratingController: ratingController),
+                  ///
+                  ///Title
+                  ///
+                  const SizedBox(height: 10),
+                  const IdeaTextFieldLabel(
+                    leftPadding: 68,
+                    label: kIdeaTextFieldTitleLabel,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(width: 8),
+                      const DiscardIdeaButton(
+                        content: kDiscardUpdateIdeaDialogContent,
+                        icon: Icons.arrow_back,
+                      ),
+                      Expanded(
+                        child: IdeaTextField(
+                          controller: titleController,
+                          hintText: kIdeaTextFieldTitleHint,
+                          margin: const EdgeInsets.only(right: 8.0),
+                        ),
+                      ),
+                      DeleteIdeaButton(idea: idea),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+
+                  ///
+                  ///Summary
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldSummaryLabel,
+                  ),
+                  IdeaTextField(
+                    controller: summaryController,
+                    hintText: kIdeaTextFieldSummaryHint,
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+
+                  ///
+                  ///Description
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldFullDescriptionLabel,
+                  ),
+                  IdeaFullDescriptionMinimized(
+                    fullDescriptionController: fullDescriptionController,
+                    descriptionFullScreenFocusNode: descriptionFullScreenFocusNode,
+                  ),
+
+                  ///
+                  ///Status and Rating
+                  ///
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      children: [
+                        IdeaStatusField(statusController: statusController),
+                        const SizedBox(width: 26),
+                        IdeaRatingField(ratingController: ratingController),
+                      ],
+                    ),
+                  ),
+
+                  ///
+                  ///Categories
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldCategories,
+                  ),
+                  const IdeaCategoriesField(),
+
+                  ///
+                  ///Revenue
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldHowAreYouGoingToEarnMoneyLabel,
+                  ),
+                  IdeaTextField(
+                    controller: revenueController,
+                    hintText: kIdeaTextFieldHowAreYouGoingToEarnMoneyHint,
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+
+                  ///
+                  ///Differentiation
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldHowAreYouDifferentLabel,
+                  ),
+                  IdeaTextField(
+                    controller: differentiationController,
+                    hintText: kIdeaTextFieldHowAreYouDifferentHint,
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+
+                  ///
+                  ///Speed
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldHowFastToLaunchLabel,
+                  ),
+                  IdeaTextField(
+                    controller: speedController,
+                    hintText: kIdeaTextFieldHowFastToLaunchHint,
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+
+                  ///
+                  ///Capital
+                  ///
+                  const SizedBox(height: 24),
+                  const IdeaTextFieldLabel(
+                    label: kIdeaTextFieldHowMuchTimeAndMoneyNeededLabel,
+                  ),
+                  IdeaTextField(
+                    controller: capitalController,
+                    hintText: kIdeaTextFieldHowMuchTimeAndMoneyNeededHint,
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+                  const SizedBox(height: 12),
                 ],
               ),
             ),
+          ),
+          const SizedBox(height: 16),
+          IdeaButton(
+            onPressed: () {
+              switch (statusController.text) {
+                case kIdeaStatusInProgress:
+                  context.read<InProgressIdeasCubit>().ideaUpdated(
+                        uid: idea.uid,
+                        title: titleController.text,
+                        summary: summaryController.text,
+                        fullDescription: fullDescriptionController.text,
+                        status: statusController.text,
+                        revenueExplanation: revenueController.text,
+                        differentiationExplanation: differentiationController.text,
+                        speedExplanation: speedController.text,
+                        capitalExplanation: capitalController.text,
+                        index: idea.index,
+                        rating: context.read<RateIdeaCubit>().state.ratingsSum,
+                        ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
+                        categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
+                        dateTimeCreated: idea.dateTimeCreated,
+                      );
+                  break;
+                case kIdeaStatusToDo:
+                  context.read<ToDoIdeasCubit>().ideaUpdated(
+                        uid: idea.uid,
+                        title: titleController.text,
+                        summary: summaryController.text,
+                        fullDescription: fullDescriptionController.text,
+                        status: statusController.text,
+                        revenueExplanation: revenueController.text,
+                        differentiationExplanation: differentiationController.text,
+                        speedExplanation: speedController.text,
+                        capitalExplanation: capitalController.text,
+                        index: idea.index,
+                        rating: context.read<RateIdeaCubit>().state.ratingsSum,
+                        ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
+                        categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
+                        dateTimeCreated: idea.dateTimeCreated,
+                      );
+                  break;
+                case kIdeaStatusDone:
+                  context.read<DoneIdeasCubit>().ideaUpdated(
+                        uid: idea.uid,
+                        title: titleController.text,
+                        summary: summaryController.text,
+                        fullDescription: fullDescriptionController.text,
+                        status: statusController.text,
+                        revenueExplanation: revenueController.text,
+                        differentiationExplanation: differentiationController.text,
+                        speedExplanation: speedController.text,
+                        capitalExplanation: capitalController.text,
+                        index: idea.index,
+                        rating: context.read<RateIdeaCubit>().state.ratingsSum,
+                        ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
+                        categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
+                        dateTimeCreated: idea.dateTimeCreated,
+                      );
+                  break;
+                case kIdeaStatusDiscarded:
+                  context.read<DiscardedIdeasCubit>().ideaUpdated(
+                        uid: idea.uid,
+                        title: titleController.text,
+                        summary: summaryController.text,
+                        fullDescription: fullDescriptionController.text,
+                        status: statusController.text,
+                        revenueExplanation: revenueController.text,
+                        differentiationExplanation: differentiationController.text,
+                        speedExplanation: speedController.text,
+                        capitalExplanation: capitalController.text,
+                        index: idea.index,
+                        rating: context.read<RateIdeaCubit>().state.ratingsSum,
+                        ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
+                        categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
+                        dateTimeCreated: idea.dateTimeCreated,
+                      );
+                  break;
+              }
 
-            ///
-            ///Categories
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldCategories,
-            ),
-            const IdeaCategoriesField(),
-
-            ///
-            ///Revenue
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldHowAreYouGoingToEarnMoneyLabel,
-            ),
-            IdeaTextField(
-              controller: revenueController,
-              hintText: kIdeaTextFieldHowAreYouGoingToEarnMoneyHint,
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-
-            ///
-            ///Differentiation
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldHowAreYouDifferentLabel,
-            ),
-            IdeaTextField(
-              controller: differentiationController,
-              hintText: kIdeaTextFieldHowAreYouDifferentHint,
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-
-            ///
-            ///Speed
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldHowFastToLaunchLabel,
-            ),
-            IdeaTextField(
-              controller: speedController,
-              hintText: kIdeaTextFieldHowFastToLaunchHint,
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-
-            ///
-            ///Capital
-            ///
-            const SizedBox(height: 24),
-            const IdeaTextFieldLabel(
-              label: kIdeaTextFieldHowMuchTimeAndMoneyNeededLabel,
-            ),
-            IdeaTextField(
-              controller: capitalController,
-              hintText: kIdeaTextFieldHowMuchTimeAndMoneyNeededHint,
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-
-            const SizedBox(height: 24),
-            IdeaButton(
-              onPressed: () {
-                switch (statusController.text) {
-                  case kIdeaStatusInProgress:
-                    context.read<InProgressIdeasCubit>().ideaUpdated(
-                          uid: idea.uid,
-                          title: titleController.text,
-                          summary: summaryController.text,
-                          fullDescription: fullDescriptionController.text,
-                          status: statusController.text,
-                          revenueExplanation: revenueController.text,
-                          differentiationExplanation: differentiationController.text,
-                          speedExplanation: speedController.text,
-                          capitalExplanation: capitalController.text,
-                          index: idea.index,
-                          rating: context.read<RateIdeaCubit>().state.ratingsSum,
-                          ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
-                          categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
-                          dateTimeCreated: idea.dateTimeCreated,
-                        );
-                    break;
-                  case kIdeaStatusToDo:
-                    context.read<ToDoIdeasCubit>().ideaUpdated(
-                          uid: idea.uid,
-                          title: titleController.text,
-                          summary: summaryController.text,
-                          fullDescription: fullDescriptionController.text,
-                          status: statusController.text,
-                          revenueExplanation: revenueController.text,
-                          differentiationExplanation: differentiationController.text,
-                          speedExplanation: speedController.text,
-                          capitalExplanation: capitalController.text,
-                          index: idea.index,
-                          rating: context.read<RateIdeaCubit>().state.ratingsSum,
-                          ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
-                          categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
-                          dateTimeCreated: idea.dateTimeCreated,
-                        );
-                    break;
-                  case kIdeaStatusDone:
-                    context.read<DoneIdeasCubit>().ideaUpdated(
-                          uid: idea.uid,
-                          title: titleController.text,
-                          summary: summaryController.text,
-                          fullDescription: fullDescriptionController.text,
-                          status: statusController.text,
-                          revenueExplanation: revenueController.text,
-                          differentiationExplanation: differentiationController.text,
-                          speedExplanation: speedController.text,
-                          capitalExplanation: capitalController.text,
-                          index: idea.index,
-                          rating: context.read<RateIdeaCubit>().state.ratingsSum,
-                          ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
-                          categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
-                          dateTimeCreated: idea.dateTimeCreated,
-                        );
-                    break;
-                  case kIdeaStatusDiscarded:
-                    context.read<DiscardedIdeasCubit>().ideaUpdated(
-                          uid: idea.uid,
-                          title: titleController.text,
-                          summary: summaryController.text,
-                          fullDescription: fullDescriptionController.text,
-                          status: statusController.text,
-                          revenueExplanation: revenueController.text,
-                          differentiationExplanation: differentiationController.text,
-                          speedExplanation: speedController.text,
-                          capitalExplanation: capitalController.text,
-                          index: idea.index,
-                          rating: context.read<RateIdeaCubit>().state.ratingsSum,
-                          ratingQuestions: context.read<RateIdeaCubit>().state.questionRatings,
-                          categories: context.read<IdeaCategoriesCubit>().state.checkedCategories,
-                          dateTimeCreated: idea.dateTimeCreated,
-                        );
-                    break;
-                }
-
-                Navigator.of(context).pop();
-              },
-              text: kUpdateIdeaButtonText,
-            ),
-            const SizedBox(height: 16),
-          ],
-        ),
+              Navigator.of(context).pop();
+            },
+            text: kUpdateIdeaButtonText,
+          ),
+          const SizedBox(height: 16),
+        ],
       );
 }
